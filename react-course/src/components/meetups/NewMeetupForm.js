de6832004210053eba9f7edf525d7a60c6e22d5d
maykaltenev <michaeltenev@gmail.com>
2022-05-29 12:31:47 +0200

    renders

--- react-course/src/components/meetups/NewMeetupForm.js
+++ react-course/src/components/meetups/NewMeetupForm.js
@@ -1,15 +1,12 @@
-import React from 'react'
-
 import Card from "../ui/Card";
-import classes from "./NewMeetupForm.module.css";
+import classes from "./NewMeetupFrom.module.css"
 
-
-function NewMeetupForm(props) {
+function NewMeetupForm() {
     return (
         <Card>
-            <form className={classes.form}>
+            <form className={classes.form} >
                 <div className={classes.control}>
-                    <label htmlFor='title'>Meetup Title</label>""
+                    <label htmlFor='title'>Meetup Title</label>
                     <input type='text' required id="title" />
                 </div>
                 <div className={classes.control}>
